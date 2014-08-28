206b7ce553b395d362a9d694da5964a04d5d65d8
Ciara Burkett <ciaraburkett@gmail.com>
2014-08-28 06:39:23 -0400

    Update for Meteor 0.9.0

--- package.js
+++ package.js
@@ -1,8 +1,11 @@
 Package.describe({
-    summary: "Bitters - Scaffold styles, variables, and structure for Bourbon projects."
+    summary: "Scaffold styles, variables, and structure for Bourbon projects.",
+    version: "0.9.0",
+    name: "ciara:bitters",
+    git: "https://github.com/CiaraBurkett/meteor-bitters.git"
 });
 
-Package.on_use(function (api) {
-    api.use(['scss','bourbon', 'neat'], ['client', 'server']);
-    //dont need to add any files to the client, they're there just so the package will build
+Package.onUse(function(api) {
+   api.versionsFrom("METEOR@0.9.0");
+   api.use(["fourseven:scss@0.95", "mrt:bourbon@3.1.8", "mrt:neat@1.5.0"], ['client', 'server']);
 });
